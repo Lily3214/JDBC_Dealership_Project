78bd5d2665a4260ee92971465a67a742e6957be6
Lily3214 <yeus1995@gmail.com>
2023-06-18 17:50:54 -0400

    phase 2, salesDao, leaseDao

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -14,14 +14,15 @@ public class LeaseDao {
 
     public int addLeaseContract(LeaseContract leaseContract) {
         // TODO: Implement the logic to add a lease contract
-        String query = "insert into lease_contracts (VIN, sale_date, price) values (?,?,?);";
+        String query = "insert into lease_contracts (VIN, lease_start, lease_end, monthly_payment) values (?,?,?,?);";
         int keyReturned = 0;
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);) {
-            preparedStatement.setString(1, salesContract.getVin());
-            preparedStatement.setDate(2, java.sql.Date.valueOf(salesContract.getSaleDate()));;
-            preparedStatement.setDouble(3, salesContract.getPrice());
+            preparedStatement.setString(1, leaseContract.getVin());
+            preparedStatement.setDate(2, java.sql.Date.valueOf(leaseContract.getLeaseStart()));
+            preparedStatement.setDate(3, java.sql.Date.valueOf(leaseContract.getLeaseEnd()));
+            preparedStatement.setDouble(4, leaseContract.getMonthlyPayment());
 
             int rows = preparedStatement.executeUpdate();
             System.out.println(rows + " rows affected!");
